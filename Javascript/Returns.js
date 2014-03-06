2718b2c3ed81999b6bacb202d792e310c7462f15
TheHackmeister <SpencerHachmeister@gmail.com>
2014-03-06 07:18:54 -0700

    Reorganization of return page has been completed.

---
+++ Javascript/Returns.js
@@ -0,0 +1,20 @@
+
+var loadReturnDetailOld = loadReturnDetail;
+var loadReturnDetail = function () {
+	ajaxCallback(reorganizeReturns);
+	loadReturnDetailOld.apply(this,arguments);
+}
+
+var reorganizeReturns = function () {
+	//This moves around the elements.
+	$('h6:contains(Location Key)').insertBefore('h6:contains(Asset ID)');
+	$('#addReturnlineLocation').insertBefore('h6:contains(Asset ID)');
+	$('<input type="text" id="addReturnLocation" rows="15">').insertBefore('h6:contains(Asset ID)');
+	$('<textarea id="addReturnTextarea" rows="15">').insertAfter('#addReturnlineAssetTag');
+	$('#addReturnlineAssetTag').hide();
+	$('#addReturnlineLocation').hide();
+}
+
+
+
+
